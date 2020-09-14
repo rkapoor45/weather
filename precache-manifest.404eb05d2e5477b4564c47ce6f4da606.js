d5dabf85cd29892de8cb43a5ae1aa28977b3338f
Vansh Luthra <vanshluthra2000@gmail.com>
2020-09-14 23:58:25 +0530

    Updates

---
+++ precache-manifest.404eb05d2e5477b4564c47ce6f4da606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a326458507501ab3de5ac776f435b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26cd52457fe9db97fd61",
+    "url": "/static/css/main.8f21884b.chunk.css"
+  },
+  {
+    "revision": "8d1d4978275482b75067",
+    "url": "/static/js/2.e38f23b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e38f23b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26cd52457fe9db97fd61",
+    "url": "/static/js/main.42173e60.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
